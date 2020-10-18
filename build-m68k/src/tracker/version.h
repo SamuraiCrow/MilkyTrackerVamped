99cb8e51fe8c3fa09ec56573bf616e50c5eeb185
André Wösten <andre@woesten.com>
2020-10-18 21:08:21 +0200

    Working on bundling and Amiga icon

--- build-m68k/src/tracker/version.h
+++ build-m68k/src/tracker/version.h
@@ -1,5 +1,5 @@
 // 0xXYYZZ = X.YY.ZZ
-const int MILKYTRACKER_VERSION = 0x10201;
+const int MILKYTRACKER_VERSION = 0x2010005;
 // Version string restricted to 20 chars for XM export!
-const char MILKYTRACKER_VERSION_STRING[] = "MilkyTracker 1.02.01";
+const char MILKYTRACKER_VERSION_STRING[] = "MilkyTracker 20.10.005";
 //                                          012345678901234567890
